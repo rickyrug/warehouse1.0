7fa27c14abb1a75feb5dfaedb877b2569ed9c23b
60044723 <60044723@TAMP00033244.tenaris.techint.net>
2015-07-21 16:04:42 -0500

    forma de valores de producto en desarrollo

---
+++ application/models/Product_Properties_Value_Model.php
@@ -0,0 +1,27 @@
+<?php
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ * Description of Product_Properties_Value_Model
+ *
+ * @author 60044723
+ */
+class Product_Properties_Value_Model extends CI_Model{
+   
+    public function get_Product_Properties_Value_Model(){}
+    
+    public function get_Product_Properties_Value_Model_fields($p_fields) {}
+    
+    public function find_by_id($p_Product_Properties_Value_Modelid){}
+    
+    public function insert_Product_Properties_Value_Model(){}
+    
+    public function update_Product_Properties_Value_Model(){}
+    
+    public function delete_Product_Properties_Value_Model($p_idProduct_Properties_Value_Model){}
+}
